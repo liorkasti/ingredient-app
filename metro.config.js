9556d0f2c7b21f7e6730fb1414c1aeca889cb770
Lior Kastenbaum <liorkasti@gmail.com>
2021-01-30 20:02:57 +0200

    Initial commit

---
+++ metro.config.js
@@ -0,0 +1,17 @@
+/**
+ * Metro configuration for React Native
+ * https://hub.com/facebook/react-native
+ *
+ * @format
+ */
+
+module.exports = {
+  transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: false,
+      },
+    }),
+  },
+};
